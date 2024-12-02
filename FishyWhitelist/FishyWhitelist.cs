66b967707e3e9157f8945face40a9d457bc31ccc
morgana <89588301+morgana-x@users.noreply.github.com>
2024-12-02 19:17:04 +1100

    Add files via upload

---
+++ FishyWhitelist/FishyWhitelist.cs
@@ -0,0 +1,70 @@
+﻿using Fishy.Extensions;
+using Fishy.Models.Packets;
+using Fishy.Models;
+namespace FishyWhitelist
+{
+    public class FishyWhitelist : FishyExtension
+    {
+
+        FishyWhitelistConfig config = new FishyWhitelistConfig();
+        public override void OnPlayerJoin(Player player)
+        {
+            if (!config.WhitelistEnabled)
+                return;
+            if (!FishyWhitelistConfig.GetWhiteList().Contains(player.SteamID.ToString()))
+                return;
+            try
+            {
+                SendPacketToPlayer(new MessagePacket(config.KickMessage, config.KickMessageColor), player.SteamID);
+                KickPlayer(player);
+                Log($"Kicked player {player.Name} {player.SteamID} for not being on whitelist!");
+            }
+            catch (Exception e)
+            {
+                LogError(e.ToString());
+            }
+        }
+        public override void OnInit()
+        {
+            refreshConfig();
+        }
+
+        private void refreshConfig()
+        {
+            FishyWhitelistConfig.GetWhiteList(); // Generate default whitelist if none exists;
+            Log("Refreshing config...");
+            string configPath = Path.Combine(Environment.CurrentDirectory, "whitelistConfig.toml");
+            if (!File.Exists(configPath))
+            {
+                File.WriteAllText(configPath, Tomlyn.Toml.FromModel(config));
+            }
+            try
+            {
+                config = Tomlyn.Toml.ToModel<FishyWhitelistConfig>(File.ReadAllText(configPath));
+            }
+            catch (Exception e)
+            {
+                LogError("couldn't read TOML!" + e.ToString());
+            }
+            LogSuccess("Refreshed config!");
+        }
+
+        public static void Log(string message, ConsoleColor color = ConsoleColor.Gray)
+        {
+            var prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("[FISHYWHITELIST] ");
+            Console.ForegroundColor = color;
+            Console.Write(message + "\n");
+            Console.ForegroundColor = prevColor;
+        }
+        public static void LogError(string message)
+        {
+            Log(message, ConsoleColor.Red);
+        }
+        public static void LogSuccess(string message)
+        {
+            Log(message, ConsoleColor.Green);
+        }
+    }
+}
---
+++ FishyWhitelist/FishyWhitelistConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace FishyWhitelist
+{
+    internal class FishyWhitelistConfig
+    {
+        public bool WhitelistEnabled { get; set; } = true;
+        public string KickMessage { get; set; } = "You are not on the server whitelist!";
+        public string KickMessageColor { get; set; } = "ff5555";
+
+        public static List<string> GetWhiteList()
+        {
+            string whiteListPath = Path.Combine(Environment.CurrentDirectory, "whitelist.txt");
+            if (!File.Exists(whiteListPath))
+                CreateDefaultWhiteList();
+            try
+            {
+                return File.ReadAllText(whiteListPath).Split("\n").ToList();
+            }
+            catch(Exception ex) 
+            {
+                FishyWhitelist.LogError( "error reading whitelist!\n" + ex);
+            }
+            return new List<string>() { };
+        }
+        private static void CreateDefaultWhiteList()
+        {
+            string whiteListPath = Path.Combine(Environment.CurrentDirectory, "whitelist.txt");
+            File.WriteAllText(whiteListPath, string.Join("\n", new string[]{ "76561197960287930", "76561197960287930" }));
+        }
+    }
+}
